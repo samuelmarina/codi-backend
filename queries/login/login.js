973687c19546fb3262223740caa8072e434ee849
samuelmarina <samuellmarina@gmail.com>
2021-05-08 20:44:58 -0400

    Pequeñas modificaciones

--- queries/login/login.js
+++ queries/login/login.js
@@ -6,14 +6,16 @@ const pool = require("../../bd/pg");
  * @param {JSON} response HTTP response
  */
 const loginUser = (request, response) => {
-    const user = request.body;
+    const user = request.body.user;
     const query = 'SELECT * FROM "User" WHERE google_id = $1';
     pool.query(query, [user.google_id],
     (err, res) => {
-        if(err) throw err;
+        if(err){
+            return err;
+        };
         if(res.rows.length === 0){
-            createUser(user);
-            return response.status(201).send('User added with ID: ${user.google_id}');
+            const newUser = createUser(user);
+            return response.status(201).send(newUser);
         }
         response.status(200).json(res.rows);
     })
@@ -26,7 +28,8 @@ const loginUser = (request, response) => {
 const createUser = (user) => {
     const query = "INSERT INTO \"User\"(google_id, name, pic_url, premium, is_admin, active) VALUES ($1, $2, $3, false, false, true)";
     pool.query(query, [user.google_id, user.name, user.pic_url], (err, res) => {
-        if(err) throw err;
+        if(err) return err;
+        return res.rows;
     });
 }
 
