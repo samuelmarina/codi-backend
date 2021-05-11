c2cc7affe47420e6335f4195f2547a4dd3579ff3
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-11 10:13:21 -0400

    comment query create payments query, it is not ready yet==> problems with trigered function

--- queries/payments/payments.js
+++ queries/payments/payments.js
@@ -5,21 +5,21 @@ const pool = require("../../bd/pg");
 
 
 
-const createPayment = (request, response) => {
-    const payment = request.body.payment;
-    const query = "INSERT INTO \"Payment\"(date,amount,pm_id,user_id,active,sub_type) VALUES ('2016-06-22 19:10:25-07',50,1,1,true,1)";
+// const createPayment = (request, response) => {
+//     const payment = request.body.payment;
+//     const query = "INSERT INTO \"Payment\"(date,amount,pm_id,user_id,active,sub_type) VALUES ('2016-06-22 19:10:25-07',50,1,1,true,1)";
    
-    // const query = 'SELECT * FROM "User" WHERE google_id = $1';
+//     // const query = 'SELECT * FROM "User" WHERE google_id = $1';
     
-    pool.query(query, (err, res) => {
-        if(err) return err;
-        return res.rows;
-    });
-    return response.status(201).send('done');
+//     pool.query(query, (err, res) => {
+//         if(err) return err;
+//         return res.rows;
+//     });
+//     return response.status(201).send('done');
 
-    // response.status(200).json(res.rows);
+//     // response.status(200).json(res.rows);
     
-}
+// }
 
 
 
