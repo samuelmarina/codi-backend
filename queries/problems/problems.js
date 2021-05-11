9a942fb02d5afb0e79c12451564b6d610dcf704c
samuelmarina <samuellmarina@gmail.com>
2021-05-10 23:21:50 -0400

    Crear función obtener problemas por dificultad

--- queries/problems/problems.js
+++ queries/problems/problems.js
@@ -6,7 +6,7 @@ const pool = require("../../bd/pg");
  * @param {JSON} response HTTP response
  */
 const getAllProblems = (request, response) => {
-    const query = 'SELECT * FROM "Problem" ORDER BY problem_id ASC'
+    const query = 'SELECT * FROM "Problem" ORDER BY problem_id ASC';
     pool.query(query, (error, results) => {
         if(error){
             return error;
@@ -15,6 +15,21 @@ const getAllProblems = (request, response) => {
     })
 }
 
+/**
+ * Obtener problemas por dificultad
+ * @param {JSON} request HTTP request
+ * @param {JSON} response HTTP response
+ */
+const getProblemsByDifficulty = (request, response) => {
+    const difficulty = request.params.difficulty;
+    const query = 'SELECT * FROM "Problem" WHERE difficulty = $1';
+    pool.query(query, [difficulty], (error, results) => {
+        if(error) return response.send(error);
+        response.status(200).json(results.rows);
+    })
+}
+
 module.exports = {
-    getAllProblems
+    getAllProblems,
+    getProblemsByDifficulty
 }
