42ca2c2bec8aa7351124170def4c17d80cf0b8f8
samuelmarina <samuellmarina@gmail.com>
2021-06-11 18:48:06 -0400

    Prueba

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({
     credentials:true
 }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', 'Content-Type');
     next();
