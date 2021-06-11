c56be478bff9c48693e9c4a90620c0974fe413fc
samuelmarina <samuellmarina@gmail.com>
2021-06-11 11:01:43 -0400

    Agregar origin a cors

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin:['http://localhost:3001'],
+    credentials:true
+}));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', 'Content-Type');
+    next();
 })
 
 const login = require('./queries/login/login');
