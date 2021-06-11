6a5eb6efdb43aa3c47d5b507043834bcd93f60da
samuelmarina <samuellmarina@gmail.com>
2021-06-11 11:02:17 -0400

    Pequeña modificación

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-    origin:['http://localhost:3001'],
+    origin:['http://localhost:3000'],
     credentials:true
 }));
 app.use((req, res, next) => {
