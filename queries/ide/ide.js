d48290aa4c0faa464348bb1abb1b7906aa0a3b34
samuelmarina <samuellmarina@gmail.com>
2021-06-22 19:18:55 -0400

    Crear función de envío de código

--- queries/ide/ide.js
+++ queries/ide/ide.js
@@ -1,4 +1,5 @@
 const pool = require("../../bd/pg");
+const dateHelper = require("../../helpers/date/date");
 const axios = require('axios');
 
 const endpoint = 'https://codexweb.netlify.app/.netlify/functions/enforceCode';
@@ -28,8 +29,97 @@ const tryCode = async (request, response) => {
         status: resultOutput === output,
         output: resultOutput,
         expectedOutput: output,
-    })
+    });
 }
+
+/**
+ * Enviar un código para evaluación
+ * @param {JSON} request HTTP request
+ * @param {JSON} response HTTP response
+ */
+const sendCode = async (request, response) => {
+    const { code, lang, problemId } = request.body.codeInfo;
+    const { userId } = request.body.user;
+    const client = await pool.connect();
+
+    const testCases = await getProblemInput(client, response, problemId);
+    let flag = true;
+    for (let i = 0; i < testCases.length; i++) {
+        const { input, output } = testCases[i];
+        const result = await executeCode(response, code, lang, input);
+        console.log(result);
+        const resultOutput = result.output.trim();
+
+        if (resultOutput !== output) {
+            flag = false;
+            break;
+        }
+    }
+
+    const submission = await createSubmission(client, response, flag, {
+        user_id: userId,
+        problem_id: problemId,
+        language: lang,
+        code: code,
+    });
+
+    response.status(201).send({
+        id: submission.id,
+        code: code,
+        language: lang,
+        status: flag ? "Aprobado" : "Desaprobado",
+        date: dateHelper.getDate()
+    });
+}
+
+/**
+ * Obtener el código de lenguaje para la librería
+ * codex
+ * @param {String} lang lenguaje de programación
+ * @returns String lenguaje para librería Codex
+ */
+const getLanguage = (lang) => {
+    switch (lang.toLowerCase()) {
+        case "java":
+            return "java"
+        case "python":
+            return "py"
+        default:
+            return "c"
+    }
+}
+
+/**
+ * Agregar una submission a la base de
+ * datos del usuario 
+ * @param {*} client 
+ * @param {*} response 
+ * @param {Boolean} solved true si el código fue resuelto
+ * @param {JSON} data info del problema y el usuario
+ * @returns Array de submission
+ */
+const createSubmission = async (client, response, solved, data) => {
+    const query = 'INSERT INTO "User-Problem"(user_id, problem_id, language, solved, active, code, date) \
+    VALUES((SELECT user_id FROM "User" WHERE google_id = $1), $2, $3, $4, TRUE, $5, $6) RETURNING id';
+
+    const {
+        user_id,
+        problem_id,
+        language,
+        code,
+    } = data;
+    
+    const date = dateHelper.getTimestamp();
+
+    try {
+        const results = await client.query(query, [user_id, problem_id, language, solved, code, date]);
+        return results.rows[0];
+    } catch (error) {
+        return response.status(400).send(error);
+    }
+}
+
+
 
 /**
  * Obtener todos los test cases de un
@@ -59,9 +149,10 @@ const getProblemInput = async (client, response, problemId) => {
  * @param {String} input code input
  */
 const executeCode = async (response, code, language, input) => {
+    const lang = getLanguage(language);
     const data = JSON.stringify({
         "code": code,
-        "language": language,
+        "language": lang,
         "input": input
     });
 
@@ -79,4 +170,5 @@ const executeCode = async (response, code, language, input) => {
 
 module.exports = {
     tryCode,
+    sendCode
 }
