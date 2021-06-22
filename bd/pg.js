76b1ac8bc7ea06566b422b8629ec2e9cda1a3d91
samuelmarina <samuellmarina@gmail.com>
2021-06-21 22:48:06 -0400

    Cambiar bd

--- bd/pg.js
+++ bd/pg.js
@@ -1,10 +1,13 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'xbzkedtk',
-    host: 'queenie.db.elephantsql.com',
-    database: 'xbzkedtk',
-    password: 'ZGbtJQnisNsstlkj9b2ieC8ArUMvm6_0',
+    user: 'awpwyxhpkcvvvj',
+    host: 'ec2-52-204-141-94.compute-1.amazonaws.com',
+    database: 'd2qnjr75k6r8c8',
+    password: '82bdfebc67ce8b08daa3aee81953a8e01556d2a054b16b6c387ce6c9b0e29df1',
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
