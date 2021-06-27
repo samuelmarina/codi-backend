2c471bb195ee50d258a1f859c1c27b7198f617b6
Rommel Sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-27 14:02:59 -0400

    add disablePremiumUser qeury function

--- queries/user/user.js
+++ queries/user/user.js
@@ -6,17 +6,31 @@ const pool = require("../../bd/pg");
  * @param {JSON} response HTTP response
  */
 const editUser = (request, response) => {
-    const user = request.body.user;
-    const query = 'UPDATE "User" SET name = $1 WHERE google_id = $2';
-    pool.query(query, [user.name, user.google_id],
-    (err, res) => {
-        if(err){
-            return response.status(400).send(error);
-        };
-        response.status(200).json("success");
-    })
-}
+  const user = request.body.user;
+  const query = 'UPDATE "User" SET name = $1 WHERE google_id = $2';
+  pool.query(query, [user.name, user.google_id], (err, res) => {
+    if (err) {
+      return response.status(400).send(error);
+    }
+    response.status(200).json("success");
+  });
+};
+
+/**
+ * Actualizar el estado de premium de un usuario
+ * @param {Number} id user_id
+ */
+const disablePremiumUser = (id) => {
+  const query = 'UPDATE "User" SET premium = FALSE WHERE user_id = $1';
+  pool.query(query, [id], (err, res) => {
+    if (err) {
+      // return response.status(400).send(error);
+    }
+    // response.status(200).json("success");
+  });
+};
 
 module.exports = {
-    editUser,
-}
+  editUser,
+  disablePremiumUser,
+};
